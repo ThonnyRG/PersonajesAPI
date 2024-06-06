eb3a91a71762d2f3b0642e9863e43626deb99db5
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-06-06 11:06:47 -0600

    first commit

---
+++ characters/src/test/java/dev/tony/characters/CharactersApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tony.characters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharactersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
