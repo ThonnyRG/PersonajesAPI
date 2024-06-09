db9424fea464c0ee0514fabca92098245efd3ae0
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-06-09 17:53:55 -0600

    Global error management

---
+++ characters/src/main/java/dev/tony/characters/RestApi/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package dev.tony.characters.RestApi;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+public class GlobalExceptionHandler {
+   @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleResponseStatusException(ResponseStatusException ex) {
+        return new ResponseEntity<>(ex.getReason(), ex.getStatusCode());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
